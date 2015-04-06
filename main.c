a8158e70a6564b83e6b95c1472ac145b5a77c8f9
Lorenzo Stoakes <lstoakes@gmail.com>
2015-04-06 17:23:41 +0100

    A major amount of deobfuscation which makes the code significantly more readable.
    This is the culmination of the work done in the first blog post in the series -
    https://ljs.io/b/2015/03/07/analysing-ascii-fluid-dynamics-i-deobfuscation
    
    This blog post documents the changes made and the rationale behind them.

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
+#include <complex.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <complex.h>
+#include <string.h>
+
+#define COMPLEX_BUFFER_SIZE  97687
+#define OUTPUT_BUFFER_SIZE   6856
+#define FRAME_SLEEP_NANO     12321
+#define FILL_RANGE_OFFSET    10
+#define FILL_RANGE_SIZE      2003
+#define ANSI_BUFFER_PROLOGUE "\x1b[2J\x1b[1;1H     "
+#define LOOKUP               " '`-.|//,\\|\\_\\/#\n"
+
+// User-definable constants.
 
 // Gravity
 #ifndef G
@@ -17,38 +28,153 @@
 #define V 8
 #endif
 
+typedef double complex cdouble;
 
-double complex a[97687], *p, *q, *r = a, w = 0, d;
-int x, y;
-char  b[6856] = "\x1b[2J" "\x1b" "[1;1H     ", *o = b, *t;
+cdouble arr[COMPLEX_BUFFER_SIZE];
+cdouble *r = arr;
+cdouble w = 0, d;
 
-int
-main()
+char buf[OUTPUT_BUFFER_SIZE] = ANSI_BUFFER_PROLOGUE;
+
+static void zero_buffer_range(void)
 {
-	for (; 0 < (x = getc(stdin));)
-		w = x > 10 ? 32 < x ? 4[*r++ = w, r] = w + 1, *r = r[5] = x == 35, r += 9 : 0, w - I : (x = w + 2);
+	memset(buf + FILL_RANGE_OFFSET, 0, FILL_RANGE_SIZE);
+}
 
-	for (; ; puts(o), o = b + 4) {
-		for (p = a; p[2] = p[1] * 9, p < r; p += 5)
-			for (q = a; w = cabs(d = *p - *q) / 2 - 1, q < r; q += 5)
-				if (0 < (x = 1 - w))
-					p[2] += w * w;
+static void format_buffer_range(void)
+{
+	for (int i = 0; i < FILL_RANGE_SIZE; i++) {
+		int j = i + FILL_RANGE_OFFSET;
 
-		for (p = a; p[3] = G, p < r; p += 5)
-			for (q = a; w = cabs(d = *p - *q) / 2 - 1, q < r; q += 5)
-				if (0 < (x = 1 - w))
-					p[3] += w * (d * (3 - p[2] - q[2]) * P + p[4] * V - q[4] * V) / p[2];
+		char old = buf[j];
 
-		for (x = 011; 2012 - 1 > x++;)
-			b[x] = 0;
+		if ((i + 1) % 80 == 0)
+			buf[j] = '\n';
+		else
+			buf[j] = LOOKUP[old];
+	}
+}
 
-		for (p = a; (t = b + 10 + (x = *p * I) + 80 * (y = *p / 2), *p += p[4] += p[3] / 10 * !p[1]), p < r; p += 5)
-			x = 0 <= x && x < 79 && 0 <= y && y < 23 ? 1[1[*t |= 8, t] |= 4, t += 80] = 1, *t |= 2 : 0;
+static void init(void)
+{
+	for (char chr = getc(stdin); chr != EOF; chr = getc(stdin)) {
+		if (chr <= '\n') {
+			w = (int)(w + 2);
+			continue;
+		}
 
-		for (x = 011; 2012 - 1 > x++;)
-			b[x] = " '`-.|//,\\" "|\\_" "\\/\x23\n"[x % 80 - 9 ? x[b] : 16];;
+		if (chr > ' ') {
+			r[0] = w;
+			r[5] = w + 1;
+			r[1] = r[6] = (chr == '#');
 
-		usleep(12321);
+			r += 10;
+		}
+
+		w -= I;
+	}
+}
+
+static void step12(void (*outer)(cdouble*, cdouble*),
+	void (*inner)(cdouble*, cdouble*), void (*inner2)(cdouble*, cdouble*))
+{
+	cdouble *p, *q;
+
+	for (p = arr; p < r; p += 5) {
+		outer(p, q);
+
+		for (q = arr; q < r; q += 5) {
+			inner(p, q);
+
+			if ((int)(1 - w) > 0)
+				inner2(p, q);
+		}
+		// TODO: Do we need to repeat this?
+		inner(p, q);
+	}
+	// TODO: Do we need to repeat this?
+	outer(p, q);
+}
+
+static void step1_outer(cdouble *p, cdouble *q)
+{
+	p[2] = p[1] * 9;
+}
+
+static void step1_inner(cdouble *p, cdouble *q)
+{
+	d = *p - *q;
+	w = cabs(d) / 2 - 1;
+}
+
+static void step1_inner2(cdouble *p, cdouble *q)
+{
+	p[2] += w * w;
+}
+
+static void step2_outer(cdouble *p, cdouble *q)
+{
+	p[3] = G;
+}
+
+static void step2_inner(cdouble *p, cdouble *q)
+{
+	d = *p - *q;
+	w = cabs(d) / 2 - 1;
+}
+
+static void step2_inner2(cdouble *p, cdouble *q)
+{
+	cdouble tmp = (3 - p[2] - q[2]) * d * P + (p[4] - q[4]) * V;
+	p[3] += w * tmp / p[2];
+}
+
+static void step4(void)
+{
+	cdouble *p, val;
+
+	for (p = arr; p < r; p += 5) {
+		val = *p;
+
+		// Truncate to ints.
+		int i = val * I;
+		int j = val / 2;
+
+		char *t = &buf[10 + i + 80 * j];
+
+		p[4] += p[3] / 10 * !p[1];
+		val += p[4];
+
+		if (0 <= i && i < 79 && 0 <= j && j < 23) {
+			t[0] |= 8;
+			t[1] |= 4;
+			i = t[80] |= 2;
+			t[81] = 1;
+		} else {
+			i = 0;
+		}
+
+		*p = val;
+	}
+	// TODO: Do we need to repeat this?
+	p[4] += p[3] / 10 * !p[1];
+	val += p[4];
+}
+
+int main(void)
+{
+	init();
+
+	for(char *out = buf; ; out = buf + 4) {
+		puts(out);
+
+		step12(step1_outer, step1_inner, step1_inner2);
+		step12(step2_outer, step2_inner, step2_inner2);
+		zero_buffer_range();
+		step4();
+		format_buffer_range();
+
+		usleep(FRAME_SLEEP_NANO);
 	}
 
 	return 0;
