5615be7a997ab52b77761ee8211d5f34fa0a3d01
Lorenzo Stoakes <lstoakes@gmail.com>
2015-04-06 17:31:58 +0100

    Use actual particle data structure rather than offsets in an array based on hint page information.

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define COMPLEX_BUFFER_SIZE  97687
+#define COMPLEX_BUFFER_SIZE  19538
 #define OUTPUT_BUFFER_SIZE   6856
 #define FRAME_SLEEP_NANO     12321
 #define FILL_RANGE_OFFSET    10
@@ -29,15 +29,21 @@
 #define V 8
 #endif
 
-typedef double complex cdouble;
+typedef double complex cdouble_t;
 
-#define FN_OUTER(__name) void (*__name)(cdouble*)
-#define FN_INNER(__name) void (*__name)(cdouble, cdouble, cdouble*, cdouble*)
+typedef struct {
+	cdouble_t pos;
+	bool is_wall;
+	cdouble_t density, force, velocity;
+} particle_t;
 
-static cdouble read_chars(cdouble **rp)
+#define FN_OUTER(__name) void (*__name)(particle_t*)
+#define FN_INNER(__name) void (*__name)(cdouble_t, cdouble_t, particle_t*, particle_t*)
+
+static int read_particles(cdouble_t *wp, particle_t *ps)
 {
-	cdouble pos = 0;
-	cdouble *r = *rp;
+	cdouble_t pos = 0;
+	int i = 0;
 
 	for (char chr = getc(stdin); chr != EOF; chr = getc(stdin)) {
 		if (chr <= '\n') {
@@ -46,94 +52,91 @@ static cdouble read_chars(cdouble **rp)
 		}
 
 		if (chr > ' ') {
-			r[0] = pos;
-			r[5] = pos + 1;
-			r[1] = r[6] = (chr == '#');
+			ps[i].pos = pos;
+			ps[i + 1].pos = pos + 1;
+			ps[i].is_wall = ps[i + 1].is_wall = (chr == '#');
 
-			r += 10;
+			i += 2;
 		}
 
 		pos -= I;
 	}
 
-	*rp = r;
-
-	return pos;
+	*wp = pos;
+	return i;
 }
 
-static void calc1(cdouble *arr, cdouble *r, cdouble *wp, FN_OUTER(outer),
+static void calc1(particle_t *arr, int len, cdouble_t *wp, FN_OUTER(outer),
 		FN_INNER(inner2))
 {
-	cdouble d = 0, w = *wp;
-	cdouble *p, *q;
+	cdouble_t d = 0, w = *wp;
 
-	for (p = arr; p < r; p += 5) {
-		outer(p);
+	for(int i = 0; i < len; i++) {
+		particle_t *particle1 = &arr[i];
 
-		for (q = arr; q < r; q += 5) {
-			d = p[0] - q[0];
+		outer(particle1);
+
+		for(int j = 0; j < len; j++) {
+			particle_t *particle2 = &arr[j];
+
+			d = particle1->pos - particle2->pos;
 			w = cabs(d) / 2 - 1;
 
 			if ((int)(1 - w) > 0)
-				inner2(d, w, p, q);
+				inner2(d, w, particle1, particle2);
 		}
 	}
 
 	*wp = w;
 }
 
-static void calc1_1_outer(cdouble *p)
+static void calc1_1_outer(particle_t *p)
 {
-	p[2] = p[1] * 9;
+	p->density = p->is_wall * 9;
 }
 
-static void calc1_1_inner2(cdouble d, cdouble w, cdouble *p, cdouble *q)
+static void calc1_1_inner2(cdouble_t d, cdouble_t w, particle_t *p, particle_t *q)
 {
-	p[2] += w * w;
+	p->density += w * w;
 }
 
-static void calc1_2_outer(cdouble *p)
+static void calc1_2_outer(particle_t *p)
 {
-	p[3] = G;
+	p->force = G;
 }
 
-static void calc1_2_inner2(cdouble d, cdouble w, cdouble *p, cdouble *q)
+static void calc1_2_inner2(cdouble_t d, cdouble_t w, particle_t *p, particle_t *q)
 {
-	cdouble tmp = (3 - p[2] - q[2]) * d * P + (p[4] - q[4]) * V;
-	p[3] += w * tmp / p[2];
+	cdouble_t tmp = (3 - p->density - q->density) * d * P +
+		(p->velocity - q->velocity) * V;
+	p->force += w * tmp / p->density;
 }
-
 
 static void zero_buffer_range(char *buf)
 {
 	memset(buf + FILL_RANGE_OFFSET, 0, FILL_RANGE_SIZE);
 }
 
-static void calc2(char *buf, cdouble *arr, cdouble *r)
+static void calc2(char *buf, particle_t *arr, int len)
 {
-	cdouble *p;
-	cdouble val;
+	for (int i = 0; i < len; i++) {
+		particle_t *particle = &arr[i];
 
-	for (p = arr; p < r; p += 5) {
-		val = *p;
+		// Intentionally truncate.
+		int x = particle->pos * I; // We store -ve, I^2 = -1.
+		int y = particle->pos / 2;
 
-		// Truncate to ints.
-		int i = val * I;
-		int j = val / 2;
+		char *t = &buf[10 + x + 80 * y];
 
-		char *t = &buf[10 + i + 80 * j];
+		particle->velocity += particle->force / 10 * !particle->is_wall;
+		particle->pos += particle->velocity;
 
-		p[4] += p[3] / 10 * !p[1];
-		val += p[4];
-
-		if (0 <= i && i < 79 && 0 <= j && j < 23) {
+		if (0 <= x && x < 79 && 0 <= y && y < 23) {
 			t[0] |= 8;
 			t[1] |= 4;
 			t[80] |= 2;
 			t[81] = 1;
 		}
-
-		*p = val;
 	}
 }
 
@@ -154,20 +157,19 @@ static void format_buffer_range(char *buf)
 int main(void)
 {
 	char buf[OUTPUT_BUFFER_SIZE] = ANSI_BUFFER_PROLOGUE;
-	cdouble arr[COMPLEX_BUFFER_SIZE];
-	cdouble *r = arr;
-	cdouble w = 0;
+	particle_t arr[COMPLEX_BUFFER_SIZE] = { { 0 } };
+	cdouble_t w = 0;
 
-	w = read_chars(&r);
+	int len = read_particles(&w, arr);
 
 	puts(buf);
 	while (1) {
 		puts(buf + 4);
 
-		calc1(arr, r, &w, calc1_1_outer, calc1_1_inner2);
-		calc1(arr, r, &w, calc1_2_outer, calc1_2_inner2);
+		calc1(arr, len, &w, calc1_1_outer, calc1_1_inner2);
+		calc1(arr, len, &w, calc1_2_outer, calc1_2_inner2);
 		zero_buffer_range(buf);
-		calc2(buf, arr, r);
+		calc2(buf, arr, len);
 		format_buffer_range(buf);
 
 		usleep(FRAME_SLEEP_NANO);
