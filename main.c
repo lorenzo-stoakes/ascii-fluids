90691c02013d2a3f62225fe4cf52f86d4a2d2e8f
Lorenzo Stoakes <lstoakes@gmail.com>
2015-04-06 17:30:50 +0100

    Found new ways to de-duplicate code, improved legibility of calculation part of code to make future analysis easier, made assumptions that didnt seem to impact behaviour.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <complex.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdbool.h>
 #include <string.h>
 
 #define COMPLEX_BUFFER_SIZE  97687
@@ -30,108 +31,88 @@
 
 typedef double complex cdouble;
 
-cdouble arr[COMPLEX_BUFFER_SIZE];
-cdouble *r = arr;
-cdouble w = 0, d;
+#define FN_OUTER(__name) void (*__name)(cdouble*)
+#define FN_INNER(__name) void (*__name)(cdouble, cdouble, cdouble*, cdouble*)
 
-char buf[OUTPUT_BUFFER_SIZE] = ANSI_BUFFER_PROLOGUE;
-
-static void zero_buffer_range(void)
+static cdouble read_chars(cdouble **rp)
 {
-	memset(buf + FILL_RANGE_OFFSET, 0, FILL_RANGE_SIZE);
-}
+	cdouble pos = 0;
+	cdouble *r = *rp;
 
-static void format_buffer_range(void)
-{
-	for (int i = 0; i < FILL_RANGE_SIZE; i++) {
-		int j = i + FILL_RANGE_OFFSET;
-
-		char old = buf[j];
-
-		if ((i + 1) % 80 == 0)
-			buf[j] = '\n';
-		else
-			buf[j] = LOOKUP[old];
-	}
-}
-
-static void init(void)
-{
 	for (char chr = getc(stdin); chr != EOF; chr = getc(stdin)) {
 		if (chr <= '\n') {
-			w = (int)(w + 2);
+			pos = (int)(pos + 2);
 			continue;
 		}
 
 		if (chr > ' ') {
-			r[0] = w;
-			r[5] = w + 1;
+			r[0] = pos;
+			r[5] = pos + 1;
 			r[1] = r[6] = (chr == '#');
 
 			r += 10;
 		}
 
-		w -= I;
+		pos -= I;
 	}
+
+	*rp = r;
+
+	return pos;
 }
 
-static void step12(void (*outer)(cdouble*, cdouble*),
-		void (*inner)(cdouble*, cdouble*), void (*inner2)(cdouble*, cdouble*))
+static void calc1(cdouble *arr, cdouble *r, cdouble *wp, FN_OUTER(outer),
+		FN_INNER(inner2))
 {
+	cdouble d = 0, w = *wp;
 	cdouble *p, *q;
 
 	for (p = arr; p < r; p += 5) {
-		outer(p, q);
+		outer(p);
 
 		for (q = arr; q < r; q += 5) {
-			inner(p, q);
+			d = p[0] - q[0];
+			w = cabs(d) / 2 - 1;
 
 			if ((int)(1 - w) > 0)
-				inner2(p, q);
+				inner2(d, w, p, q);
 		}
-		// TODO: Do we need to repeat this?
-		inner(p, q);
 	}
-	// TODO: Do we need to repeat this?
-	outer(p, q);
+
+	*wp = w;
 }
 
-static void step1_outer(cdouble *p, cdouble *q)
+static void calc1_1_outer(cdouble *p)
 {
 	p[2] = p[1] * 9;
 }
 
-static void step1_inner(cdouble *p, cdouble *q)
-{
-	d = *p - *q;
-	w = cabs(d) / 2 - 1;
-}
-
-static void step1_inner2(cdouble *p, cdouble *q)
+static void calc1_1_inner2(cdouble d, cdouble w, cdouble *p, cdouble *q)
 {
 	p[2] += w * w;
 }
 
-static void step2_outer(cdouble *p, cdouble *q)
+static void calc1_2_outer(cdouble *p)
 {
 	p[3] = G;
 }
 
-static void step2_inner(cdouble *p, cdouble *q)
-{
-	d = *p - *q;
-	w = cabs(d) / 2 - 1;
-}
-
-static void step2_inner2(cdouble *p, cdouble *q)
+static void calc1_2_inner2(cdouble d, cdouble w, cdouble *p, cdouble *q)
 {
 	cdouble tmp = (3 - p[2] - q[2]) * d * P + (p[4] - q[4]) * V;
 	p[3] += w * tmp / p[2];
 }
 
-static void step4(void)
+
+static void zero_buffer_range(char *buf)
 {
-	cdouble *p, val;
+	memset(buf + FILL_RANGE_OFFSET, 0, FILL_RANGE_SIZE);
+}
+
+static void calc2(char *buf, cdouble *arr, cdouble *r)
+{
+	cdouble *p;
+	cdouble val;
 
 	for (p = arr; p < r; p += 5) {
 		val = *p;
@@ -148,31 +129,46 @@ static void step4(void)
 		if (0 <= i && i < 79 && 0 <= j && j < 23) {
 			t[0] |= 8;
 			t[1] |= 4;
-			i = t[80] |= 2;
+			t[80] |= 2;
 			t[81] = 1;
-		} else {
-			i = 0;
 		}
 
 		*p = val;
 	}
-	// TODO: Do we need to repeat this?
-	p[4] += p[3] / 10 * !p[1];
-	val += p[4];
+}
+
+static void format_buffer_range(char *buf)
+{
+	for (int i = 0; i < FILL_RANGE_SIZE; i++) {
+		int j = i + FILL_RANGE_OFFSET;
+
+		int old = buf[j];
+
+		if ((i + 1) % 80 == 0)
+			buf[j] = '\n';
+		else
+			buf[j] = LOOKUP[old];
+	}
 }
 
 int main(void)
 {
-	init();
+	char buf[OUTPUT_BUFFER_SIZE] = ANSI_BUFFER_PROLOGUE;
+	cdouble arr[COMPLEX_BUFFER_SIZE];
+	cdouble *r = arr;
+	cdouble w = 0;
 
-	for (char *out = buf; ; out = buf + 4) {
-		puts(out);
+	w = read_chars(&r);
 
-		step12(step1_outer, step1_inner, step1_inner2);
-		step12(step2_outer, step2_inner, step2_inner2);
-		zero_buffer_range();
-		step4();
-		format_buffer_range();
+	puts(buf);
+	while (1) {
+		puts(buf + 4);
+
+		calc1(arr, r, &w, calc1_1_outer, calc1_1_inner2);
+		calc1(arr, r, &w, calc1_2_outer, calc1_2_inner2);
+		zero_buffer_range(buf);
+		calc2(buf, arr, r);
+		format_buffer_range(buf);
 
 		usleep(FRAME_SLEEP_NANO);
 	}
